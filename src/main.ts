a2d44ff58b32b35a0a92264765cf534def4c03a2
Sujackson <su_jackson99@yahoo.com>
2025-01-15 12:45:55 +0700

    server

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as https from 'https';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: 'http://localhost:3000',
     credentials: true,
   });
   await app.listen(3020);
