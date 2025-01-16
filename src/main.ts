b0fd9ada4b3f1afb29650add6bf84e7508f340f3
Sujackson <su_jackson99@yahoo.com>
2025-01-16 14:46:40 +0700

    server

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,20 @@ import * as https from 'https';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://16.78.223.213:3000',
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        'http://16.78.223.213:3000',
+        'http://dashboard.sms123.online',
+      ];
+
+      // Allow requests with no origin (like mobile apps or Postman)
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+
     credentials: true,
   });
   await app.listen(3020);
