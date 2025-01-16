e8ec0cbf8eafabb2b5f324500ca89d0eb33742de
Sujackson <su_jackson99@yahoo.com>
2025-01-16 15:30:03 +0700

    server

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,22 +1,15 @@
-import { Controller, Get, Post, Request, Res, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from './jwt-auth.guard';
 import { LocalAuthGuard } from './local-auth.guard';
-import { Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
   @UseGuards(LocalAuthGuard)
   @Post('login')
-  async login(@Res() res: Response, @Request() req): Promise<any> {
-    const user = await this.authService.generateToken(req.user);
-    // res.cookie('nextauth.token', user.access_token, {
-    //   // httpOnly: true,
-    //   // secure: true, // Ensure this is true for HTTPS
-    //   sameSite: 'lax', // Required for cross-origin cookies
-    // });
-    return user;
+  async login(@Request() req): Promise<any> {
+    return this.authService.generateToken(req.user);
   }
 
   @UseGuards(JwtAuthGuard)
